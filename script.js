007d08d1b0fe31e876434fc4270251958acf167a
pika <amari@fluent.lol>
2024-11-19 19:58:07 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('screen1').addEventListener('click', () => {
+    document.getElementById('screen1').classList.add('hidden');
+    document.getElementById('screen2').classList.remove('hidden');
+});
